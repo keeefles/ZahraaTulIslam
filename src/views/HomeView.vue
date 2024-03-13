f849fdbc529193215c3da0b8ea8b7b7f886e7ea9
Aakeefah Jones <aakeefahj@gmail.com>
2024-03-13 06:35:51 +0200

    still fixing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="home">
-   
+  <div class="container-fluid">
+    <div class="col">
+      this is the home page.
+      <div class="button">
+        <button>
+          <a href="../views/RegisterView.vue">Login or Register here</a>
+        </button>
+      </div>
+    </div>
+    <div class="col"></div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
-
+        <h2>hello there</h2>
+        <input type="text" class="registerCon" required placeholder="insert first name">
+        </input>
     </div>
 </template>
 
+f
 <script>
     export default {
         
