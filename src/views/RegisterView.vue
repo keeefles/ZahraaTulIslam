5901cd55682f08d8436e7ea3c3a4a5fd63dbdc64
Aakeefah Jones <aakeefahj@gmail.com>
2024-03-16 10:31:16 +0200

    signup complete

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,78 +1,124 @@
 <template>
-  <form>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="First Name"
-        v-model="payload.firstName"
-      />
+  <!-- <div class="container">
+    <h2 class="head">Sign Up</h2>
+    <form class="formCont">
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="First Name"
+          v-model="payload.firstName"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Last Name"
+          v-model="payload.lastName"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Age"
+          v-model="payload.userAge"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Username"
+          v-model="payload.username"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Email Address"
+          v-model="payload.emailAdd"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Mobile Number"
+          v-model="payload.mobileNumber"
+        />
+      </div>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="User Password"
+          v-model="payload.userPass"
+        />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+          Close
+        </button>
+        <button
+          type="submit"
+          @click.prevent="addingUser"
+          class="btn btn-success"
+          data-bs-dismiss="modal"
+        >
+          Add User
+        </button>
+      </div>
+    </form>
+  </div> -->
+  <div class="outerCont">
+    <div class="form-container">
+      <p class="title">Create an account</p>
+      <form class="form">
+        <input type="text" 
+          class="input" 
+          placeholder="First Name"
+          v-model="payload.firstName" />
+        <input type="text" 
+          class="input" 
+          placeholder="Last Name"
+          v-model="payload.lastName" />
+        <input type="text" 
+          class="input" 
+          placeholder="Age"
+          v-model="payload.userAge" />
+        <input type="text" 
+          class="input" 
+          placeholder="Username"
+          v-model="payload.username" />
+        <input type="email" 
+          class="input" 
+          placeholder="Email Address"
+          v-model="payload.emailAdd" />
+        <input type="text"
+          class="input" 
+          placeholder="Mobile Number"
+          v-model="payload.mobileNumber" />
+        <input type="password" 
+          class="input" 
+          placeholder="Password"
+          v-model="payload.userPass" />
+        <button class="form-btn"
+          type="submit"
+          @click.prevent="addingUser"
+          >
+          Create account
+        </button>
+      </form>
+      <p class="sign-up-label">
+        Already have an account? <a href="./LoginView.vue" class="sign-up-link">Log in</a>
+      </p>
     </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="Last Name"
-        v-model="payload.lastName"
-      />
-    </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="Age"
-        v-model="payload.userAge"
-      />
-    </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="Username"
-        v-model="payload.username"
-      />
-    </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="Email Address"
-        v-model="payload.emailAdd"
-      />
-    </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="Mobile Number"
-        v-model="payload.mobileNumber"
-      />
-    </div>
-    <div class="mb-3">
-      <input
-        type="text"
-        class="form-control w-50 mx-auto"
-        placeholder="User Password"
-        v-model="payload.userPass"
-      />
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-        Close
-      </button>
-      <button
-        type="submit"
-        @click.prevent="addingUser"
-        class="btn btn-success"
-        data-bs-dismiss="modal"
-      >
-        Add User
-      </button>
-    </div>
-  </form>
+  </div>
 </template>
 
-f
 <script>
 export default {
   name: "addUser",
@@ -102,4 +148,105 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/* input {
+  border: 1px #999 solid;
+  border-radius: 15px;
+  width: 280px;
+  display: inline-block;
+  justify-content: center;
+}
+.container {
+  padding: 50px;
+  background-image: url("https://iili.io/JWpzVjt.md.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.head {
+  margin-bottom: 20px;
+  background-color: white;
+  width: 100px;
+} */
+.outerCont {
+  padding: 20px;
+}
+.form-container {
+  width: 350px;
+  height: 680px;
+  background-color: #fff;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  border-radius: 10px;
+  box-sizing: border-box;
+  padding: 30px 30px;
+  display: inline-block;
+}
+
+.title {
+  text-align: center;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  margin: 10px 0 30px 0;
+  font-size: 28px;
+  font-weight: 800;
+}
+
+.sub-title {
+  margin: 0;
+  margin-bottom: 5px;
+  font-size: 9px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.form {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 18px;
+  margin-bottom: 15px;
+}
+
+.input {
+  border-radius: 20px;
+  border: 1px solid #c0c0c0;
+  outline: 0 !important;
+  box-sizing: border-box;
+  padding: 12px 15px;
+}
+
+.form-btn {
+  padding: 10px 15px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  border-radius: 20px;
+  border: 0 !important;
+  outline: 0 !important;
+  background: teal;
+  color: white;
+  cursor: pointer;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.form-btn:active {
+  box-shadow: none;
+}
+
+.sign-up-label {
+  margin: 0;
+  font-size: 10px;
+  color: #747474;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.sign-up-link {
+  margin-left: 1px;
+  font-size: 11px;
+  text-decoration: underline;
+  text-decoration-color: teal;
+  color: teal;
+  cursor: pointer;
+  font-weight: 800;
+}
+</style>
